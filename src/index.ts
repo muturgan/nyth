7e49ddcfdbf7bb93c76eda709ad8f86431f038b3
Andrey Sakharov <sakharov.a@n-t.io>
2022-01-12 23:14:10 +0300

    a basic RPC argument parsing was implemented

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,72 @@
 import http = require('http');
+import qs = require('querystring');
 
 
 
 const GET = 'GET';
 const POST = 'POST';
-const DUMMY_FAVICON = '/favicon.ico';
+const QUERY_SEPARATOR = '?';
+const API = '/api';
 
 
-const server = http.createServer(async (req, res): Promise<void> => {
-
-   if (req.url === undefined) {
+const server = http.createServer(async (req, res): Promise<void> =>
+{
+   if (typeof req.url !== 'string') {
       res.socket?.destroy();
       return;
    }
 
-   switch (req.method) {
+   const [pathName, searchStr] = req.url.split(QUERY_SEPARATOR);
 
+   if (pathName !== API) {
+      res.socket?.destroy();
+      return;
+   }
+
+
+   let rpcReq: Record<string, unknown>;
+
+   switch (req.method)
+   {
       case GET:
-
-         if (req.url === DUMMY_FAVICON) {
-            res.writeHead(204);
-            res.end();
-            return;
-         }
-
-         if (req.url === '/hello') {
-            res.end('Hello World!');
-            return;
-         }
-
-         res.socket?.destroy();
-         return;
+         rpcReq = {...qs.parse(searchStr)};
+         break;
 
 
       case POST:
-         res.socket?.destroy();
-         return;
+         const chunks: Buffer[] = [];
+         try {
+            for await (const chunk of req) {
+               chunks.push(chunk);
+            }
+         } catch (err) {
+            console.info('Connection error!');
+            console.info(err);
+            res.writeHead(500);
+            res.end('Connection error');
+            return;
+         }
 
+         const body = Buffer.concat(chunks).toString();
+         try {
+            rpcReq = JSON.parse(body);
+         } catch {
+            res.writeHead(400);
+            res.end('Incorrect request body. It should be a valid json-serialized object');
+            return;
+         }
+         break;
 
       default:
          res.socket?.destroy();
          return;
    }
+
+   console.log('rpcReq:');
+   console.log(rpcReq);
+
+   res.end('Hello World!');
+   return;
 
 });
 
