ae7cae84750ac951162e7b96071b94d6bee7d745
Andrey Sakharov <sakharov.a@n-t.io>
2023-06-06 13:55:32 +0300

    0.3.1 @nyth/common es6 export fixed

--- packages/common/es6.mjs
+++ packages/common/es6.mjs
@@ -1,1 +1,18 @@
-export {} from './dist/index.cjs';
+export {
+    BaseAdapter,
+    INTERNAL_ERROR_MESSAGE,
+    PERMISSIONS_DENIED_MESSAGE,
+    AppError,
+    ScenarioError,
+    PermissionsDeniedError,
+    SystemError,
+    HandlerValidationSuccess,
+    HANDLER_VALIDATION_SUCCESS,
+    HandlerValidationFail,
+    RpcResult,
+    ScenarioSuccessResult,
+    PermissionsDeniedResult,
+    ScenarioFailResult,
+    SystemErrorResult,
+    defaultSerializer
+} from './dist/index.cjs';
